cb7213981bcc190b74d85b97ef82545a3b20436b
congsenpai <congcoihv5@gmail.com>
2025-01-14 16:08:30 +0700

    Init Project

---
+++ Backend/smartbrain/src/main/java/com/smartparking/smartbrain/SmartbrainApplication.java
@@ -0,0 +1,13 @@
+package com.smartparking.smartbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbrainApplication.class, args);
+	}
+
+}
