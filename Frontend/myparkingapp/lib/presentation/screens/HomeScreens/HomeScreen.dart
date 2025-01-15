0a8450e2085b42fc75a9a8c931ae9c8fa191ba48
My name is Bảo <123916070+tuilaBao03@users.noreply.github.com>
2025-01-15 15:01:11 +0700

    update language and color

---
+++ Frontend/myparkingapp/lib/presentation/screens/HomeScreens/HomeScreen.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: file_names, use_key_in_widget_constructors, library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Home Screen!'),
+      ),
+    );
+  }
+}
