a5da509c516441aec92904aa2564129979f26648
My name is Bảo <123916070+tuilaBao03@users.noreply.github.com>
2025-05-04 22:37:15 +0700

    Update TransactionController.java

--- Backend/smartbrain/src/main/java/com/smartparking/smartbrain/controller/TransactionController.java
+++ Backend/smartbrain/src/main/java/com/smartparking/smartbrain/controller/TransactionController.java
@@ -14,12 +14,6 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.data.domain.PageRequest;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-=======
->>>>>>> main
 
 @RestController
 @RequestMapping("/myparkingapp/transactions")
